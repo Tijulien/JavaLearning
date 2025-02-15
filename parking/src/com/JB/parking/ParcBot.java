113cfe88d1439fe017fa0b65bddc2f296093b3bd
Tijulien BAPTISTE <herby94@hotmail.com>
2025-02-15 17:30:32 +0100

    Sauvegarde des modifications de la branche p1c3 renommée en p1c3v1

--- parking/src/com/JB/parking/ParcBot.java
+++ parking/src/com/JB/parking/ParcBot.java
@@ -1,8 +1,26 @@
 package com.JB.parking;
 
 public class ParcBot {
+	/**
+	 * Ceci la fonction principale d'un programme JAVA
+	 * @param args Représente les données passées en paramètre lors du lancement du programme
+	 */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+    	sayHello(args);	
     }
+
+	/**
+	 * Permet d'accueillir et de présenter des services l'utilisateurs potentiel du Parking 
+	 * @param args Représente le nom de l'utilisateur potentiel
+	 */
+	private static void sayHello(String[] args) {
+		String welcomeMessage = "Hello " + args[0] + " , Binvenue au Parking Big Show";
+    	String services = "Nous offrons les services suivants: Gardinage, Laverie...";
+    	String warning = "Places restantes: 20!";
+    	 System.out.println(welcomeMessage);
+    	 System.out.println(services);
+    	 System.out.println(warning.toUpperCase());
+	}
+   
 }
 
